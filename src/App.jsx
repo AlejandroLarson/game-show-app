a4e7ea301359dcba55b667a006e97415f53e9707
alejandrolarson <alejandrolarson1@yahoo.com>
2025-07-07 01:03:38 -0700

    debugging game screen

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,19 @@ import './App.css'
 import MainMenu from './MainMenu'
 import IntroScreen from './IntroScreen'
 import GameScreen from './GameScreen'
+import scenes from './assets/scenes.js'
 
 function App() {
   // Our game states will be menu, intro, game, trivia, feedback, gamewin, gamelose
   const [gameState, setGameState] = useState('menu');
-  const [lives, setLives] = useState(3);
+  const [lives, setLives] = useState(2);
   const [difficulty, setDifficulty] = useState('easy');
   const [userCategoryChoice, setUserCategoryChoice] = useState(null);
+
+  // Have a list of scenes to follow, index 0-5 inclusive and stored in the scenes.js file
+const [currentSceneIndex, setCurrentSceneIndex] = useState(0);
+const [currentScene, setCurrentScene] = useState(scenes[currentSceneIndex]);
+
   
   // Data is a question we fetch from trivia API
   const [data, setData] = useState([]);
@@ -25,9 +31,10 @@ function App() {
     (<IntroScreen setGameState={setGameState}/>)}
 
     {gameState === "game" &&
-    (<GameScreen lives = {lives} setLives={setLives} />)}
+    (<GameScreen currentScene = {currentScene} setCurrentSceneIndex={setCurrentSceneIndex}
+     lives = {lives} setLives={setLives} userCategoryChoice={userCategoryChoice} setUserCategoryChoice={setUserCategoryChoice} />)}
 
-    
+
     </>
 
 );
--- src/GameScreen.jsx
+++ src/GameScreen.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import {categoryMap, getCategoryNameById} from './Helpers.js';
 
-export default function GameScreen({currentObstacle, setGameState, lives}){
+export default function GameScreen({currentScene, setCurrentSceneIndex, lives, setLives, userCategoryChoice, setUserCategoryChoice}){
+
+    if(!currentScene) return <div>Loading...</div>
+
     return(
-        <h1></h1>
+        <div className = "game-screen">
+            <div className = "game-header">
+                <h2>Detective Mittens</h2>
+                <div className="lives-counter">Lives: {lives}</div>
+            </div>
+            
+            <div classname = "scene-info">
+                <h3>{currentScene.title}</h3>
+                <p>{currentScene.description}</p>
+            </div>
+
+            <div className = "category-choices">
+                <p>Choose a trivia category</p>
+                {currentScene.categories.map((id) => (
+                    <button key = "categoryId"
+                    onClick = {()=> setUserCategoryChoice(categoryId)}>
+                        {getCategoryNameById(categoryId)}
+                    </button>
+                ))}
+            </div>
+
+        </div>
     );
 }
